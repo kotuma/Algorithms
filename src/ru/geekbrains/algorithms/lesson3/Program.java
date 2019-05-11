5550877a6087b71f446ae3aa26c9cd4eace69164
Kotuma Alexander <aikotuma@gmail.com>
2019-05-11 17:04:53 +0400

    Lesson3 - class Three and Program implemented

---
+++ src/ru/geekbrains/algorithms/lesson3/Program.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.algorithms.lesson3;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println(Three.class.getSimpleName());
+        System.out.println("args = [" + new Three().getW() + "]");
+    }
+
+}
---
+++ src/ru/geekbrains/algorithms/lesson3/Three.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.algorithms.lesson3;
+
+public class Three {
+    private int w = 1;
+
+    public Three() {
+        w = 3;
+    }
+
+    public int getW() {
+        return w;
+    }
+}
