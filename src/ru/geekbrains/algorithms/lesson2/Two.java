dec49d83c60e67fd2137ed61722a98bbbc6981d8
Kotuma Alexander <aikotuma@gmail.com>
2019-05-11 17:12:09 +0400

    Lesson2 - class Two modified

--- src/ru/geekbrains/algorithms/lesson2/Two.java
+++ src/ru/geekbrains/algorithms/lesson2/Two.java
@@ -2,8 +2,17 @@ package ru.geekbrains.algorithms.lesson2;
 
 public class Two {
     private int x = 1;
-    private int y = 1;
-    private int z = 1;
+    private int y = 2;
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    private int z = 2;
 
     public Two() {
         x = 222;
