ca19ce6a24b13788f00c4d723f93afee83d335bf
Kotuma Alexander <aikotuma@gmail.com>
2019-05-11 16:54:02 +0400

    Lesson2 - classes Two and Program was implemented

---
+++ src/ru/geekbrains/algorithms/lesson2/Program.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.algorithms.lesson2;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("args = [" + new Two().getX() + "]");
+    }
+
+}
---
+++ src/ru/geekbrains/algorithms/lesson2/Two.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.algorithms.lesson2;
+
+public class Two {
+    private int x = 1;
+    private int y = 1;
+
+    public Two() {
+        x = 222;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
