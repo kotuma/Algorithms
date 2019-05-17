adf6e144f7099bf3cbc392b2f3a914874f22e449
Kotuma Alexander <aikotuma@gmail.com>
2019-05-17 19:41:27 +0400

    Lesson5 - MyDegree finished

--- src/ru/geekbrains/algorithms/lesson5/MyDegree.java
+++ src/ru/geekbrains/algorithms/lesson5/MyDegree.java
@@ -1,6 +1,27 @@
 package ru.geekbrains.algorithms.lesson5;
 
 public class MyDegree {
-    //
+    public static int degree(int a, int degree){
+        int res = a;
+        if (degree == 0) {
+            return 1;
+        }
+        if (degree == 1) {
+            return res;
+        }
+        int n = 2;
+        int half = a;
+        while (n <= degree) {
+            if (n % 2 == 0) {
+                res = half * half;
+            }
+            else {
+                half = half * a;
+                res *= a;
+            }
+            n++;
+        }
+        return res;
 
+    }
 }
--- src/ru/geekbrains/algorithms/lesson5/Program5.java
+++ src/ru/geekbrains/algorithms/lesson5/Program5.java
@@ -2,6 +2,19 @@ package ru.geekbrains.algorithms.lesson5;
 
 public class Program5 {
     public static void main(String[] args) {
-        System.out.println("args = [  ]");
+        System.out.println(MyDegree.degree(3, 2) );
+        System.out.println(MyDegree.degree(3, 3) );
+        System.out.println(MyDegree.degree(3, 4) );
+        System.out.println(MyDegree.degree(3, 5) );
+        System.out.println(MyDegree.degree(3, 6) );
+        System.out.println(MyDegree.degree(3, 7) );
+        System.out.println(MyDegree.degree(3, 8) );
+        System.out.println(MyDegree.degree(3, 9) );
+        System.out.println(MyDegree.degree(3, 10) );
+        System.out.println(MyDegree.degree(3, 11) );
+        System.out.println(MyDegree.degree(3, 12) );
+        System.out.println(MyDegree.degree(3, 13) );
+        System.out.println(MyDegree.degree(3, 14) );
+
     }
 }
